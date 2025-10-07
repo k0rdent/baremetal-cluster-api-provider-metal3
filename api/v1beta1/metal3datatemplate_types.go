metal3-io/cluster-api-provider-metal3

Add parameters blob for bond network interface definition
Cloud-init allows to define a wide list of parameters for network bond
interfaces. But definition of `NetworkDataLinkBond` type do not allow to
pass externded set of parameters via network-data. This change adds
"params" blob/dict into `NetworkDataLinkBond` object that can be used to
pass any set of bond parameters.

Signed-off-by: Dmitriy Bogun <nyaka@nyaka.org>

api/v1beta1/metal3datatemplate_types.go
@@ -19,6 +19,7 @@ package v1beta1
 import (
 	ipamv1 "github.com/metal3-io/ip-address-manager/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -264,10 +265,16 @@ type NetworkDataLinkBond struct {
 	// used to render it.
 	MACAddress *NetworkLinkEthernetMac `json:"macAddress"`
 
+	// params blob passed without any validation/modifications into cloud-init config
+	Params NetworkDataLinkBondParams `json:"params,omitempty"`
+
 	// BondLinks is the list of links that are part of the bond.
 	// +optional
 	BondLinks []string `json:"bondLinks"`
 }
+
+// NetworkDataLinkBondParams represent the set of bond params.
+type NetworkDataLinkBondParams map[string]apiextensionsv1.JSON
 
 // NetworkDataLinkVlan represents a vlan link object.
 type NetworkDataLinkVlan struct {

api/v1beta1/zz_generated.deepcopy.go
@@ -1507,6 +1507,13 @@ func (in *NetworkDataLinkBond) DeepCopyInto(out *NetworkDataLinkBond) {
 		*out = new(NetworkLinkEthernetMac)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Params != nil {
+		in, out := &in.Params, &out.Params
+		*out = make(NetworkDataLinkBondParams, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
 	if in.BondLinks != nil {
 		in, out := &in.BondLinks, &out.BondLinks
 		*out = make([]string, len(*in))
@@ -1522,6 +1529,27 @@ func (in *NetworkDataLinkBond) DeepCopy() *NetworkDataLinkBond {
 	out := new(NetworkDataLinkBond)
 	in.DeepCopyInto(out)
 	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in NetworkDataLinkBondParams) DeepCopyInto(out *NetworkDataLinkBondParams) {
+	{
+		in := &in
+		*out = make(NetworkDataLinkBondParams, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkDataLinkBondParams.
+func (in NetworkDataLinkBondParams) DeepCopy() NetworkDataLinkBondParams {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkDataLinkBondParams)
+	in.DeepCopyInto(out)
+	return *out
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.

baremetal/metal3data_manager.go
@@ -992,7 +992,7 @@ func renderNetworkLinks(networkLinks infrav1.NetworkDataLink,
 		if err != nil {
 			return nil, err
 		}
-		data = append(data, map[string]interface{}{
+		entry := map[string]interface{}{
 			"type":                  "bond",
 			"id":                    link.Id,
 			"mtu":                   link.MTU,
@@ -1000,7 +1000,25 @@ func renderNetworkLinks(networkLinks infrav1.NetworkDataLink,
 			"bond_mode":             link.BondMode,
 			"bond_xmit_hash_policy": link.BondXmitHashPolicy,
 			"bond_links":            link.BondLinks,
-		})
+		}
+
+		illegalParams := []string{}
+		for opt, value := range link.Params {
+			target := "bond_" + opt
+			_, ok := entry[target]
+			if ok {
+				illegalParams = append(illegalParams, opt)
+				continue // Do not overwrite structured parameters
+			}
+			entry[target] = value
+		}
+
+		if 0 < len(illegalParams) {
+			return nil, fmt.Errorf("illegal params \"%s\" into bond params blob into link id=%s",
+				strings.Join(illegalParams, `", "`), link.Id)
+		}
+
+		data = append(data, entry)
 	}
 
 	// Ethernet links

k0rdent/baremetal-cluster-api-provider-metal3

api/v1beta1/metal3datatemplate_types.go
@@ -19,6 +19,7 @@ package v1beta1
 import (
 	ipamv1 "github.com/metal3-io/ip-address-manager/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -264,10 +265,16 @@ type NetworkDataLinkBond struct {
 	// used to render it.
 	MACAddress *NetworkLinkEthernetMac `json:"macAddress"`
 
+	// params blob passed without any validation/modifications into cloud-init config
+	Parameters NetworkDataLinkBondParams `json:"parameters,omitempty"`
+
 	// BondLinks is the list of links that are part of the bond.
 	// +optional
 	BondLinks []string `json:"bondLinks"`
 }
+
+// NetworkDataLinkBondParams represent the set of bond params.
+type NetworkDataLinkBondParams map[string]apiextensionsv1.JSON
 
 // NetworkDataLinkVlan represents a vlan link object.
 type NetworkDataLinkVlan struct {

api/v1beta1/zz_generated.deepcopy.go
@@ -1497,6 +1497,13 @@ func (in *NetworkDataLinkBond) DeepCopyInto(out *NetworkDataLinkBond) {
 		*out = new(NetworkLinkEthernetMac)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Parameters != nil {
+		in, out := &in.Parameters, &out.Parameters
+		*out = make(NetworkDataLinkBondParams, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
 	if in.BondLinks != nil {
 		in, out := &in.BondLinks, &out.BondLinks
 		*out = make([]string, len(*in))
@@ -1512,6 +1519,27 @@ func (in *NetworkDataLinkBond) DeepCopy() *NetworkDataLinkBond {
 	out := new(NetworkDataLinkBond)
 	in.DeepCopyInto(out)
 	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in NetworkDataLinkBondParams) DeepCopyInto(out *NetworkDataLinkBondParams) {
+	{
+		in := &in
+		*out = make(NetworkDataLinkBondParams, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkDataLinkBondParams.
+func (in NetworkDataLinkBondParams) DeepCopy() NetworkDataLinkBondParams {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkDataLinkBondParams)
+	in.DeepCopyInto(out)
+	return *out
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.

baremetal/metal3data_manager.go
@@ -992,7 +992,7 @@ func renderNetworkLinks(networkLinks infrav1.NetworkDataLink,
 		if err != nil {
 			return nil, err
 		}
-		data = append(data, map[string]interface{}{
+		entry := map[string]interface{}{
 			"type":                  "bond",
 			"id":                    link.Id,
 			"mtu":                   link.MTU,
@@ -1000,7 +1000,25 @@ func renderNetworkLinks(networkLinks infrav1.NetworkDataLink,
 			"bond_mode":             link.BondMode,
 			"bond_xmit_hash_policy": link.BondXmitHashPolicy,
 			"bond_links":            link.BondLinks,
-		})
+		}
+
+		illegalParameters := []string{}
+		for opt, value := range link.Parameters {
+			target := "bond_" + opt
+			_, ok := entry[target]
+			if ok {
+				illegalParameters = append(illegalParameters, opt)
+				continue // Do not overwrite structured parameters
+			}
+			entry[target] = value
+		}
+
+		if 0 < len(illegalParameters) {
+			return nil, fmt.Errorf("illegal params \"%s\" into bond params blob into link id=%s",
+				strings.Join(illegalParameters, `", "`), link.Id)
+		}
+
+		data = append(data, entry)
 	}
 
 	// Ethernet links